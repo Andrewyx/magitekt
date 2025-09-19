75d384a8575cb83eecf861ecbad5dfe63735cc31
Andrew Mao <72371419+Andrewyx@users.noreply.github.com>
2025-09-19 01:19:33 -0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+
+import {NFCComponent} from './Components/nfc'
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAT4F_oc8iX6wKBXchueyhV12H7Ux3smU4",
+  authDomain: "magitekt.firebaseapp.com",
+  projectId: "magitekt",
+  storageBucket: "magitekt.firebasestorage.app",
+  messagingSenderId: "619766633965",
+  appId: "1:619766633965:web:99b5112bbf4ddedf8e03e4"
+};
+
+// Initialize Firebase
+// @ts-ignore
+const app = initializeApp(firebaseConfig);
+
+function App() {
+  // @ts-ignore
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <NFCComponent />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/nfc.tsx
@@ -0,0 +1,48 @@
+
+export function NFCComponent() {
+
+  async function onScanClick() {
+
+    console.log("User clicked scan button");
+
+    try {
+      // @ts-ignore
+      const ndef = new NDEFReader();
+      await ndef.scan();
+      console.log("> Scan started");
+
+      ndef.addEventListener("readingerror", () => {
+        console.log("Argh! Cannot read data from the NFC tag. Try another one?");
+      });
+
+      // @ts-ignore
+      ndef.addEventListener("reading", ({ message, serialNumber }) => {
+        console.log(`> Serial Number: ${serialNumber}`);
+        console.log(`> Records: (${message.records.length})`);
+      });
+    } catch (error) {
+      console.log("Argh! " + error);
+    }
+
+  }
+  async function onWriteClick() {
+    console.log("User clicked write button");
+
+    try {
+      // @ts-ignore
+      const ndef = new NDEFReader();
+      await ndef.write("Hello world!");
+      console.log("> Message written");
+    } catch (error) {
+      console.log("Argh! " + error);
+    }
+  }
+
+  return (
+    <div>
+      <h1>NFC Demo</h1>
+      <button id="scanButton" onClick={onScanClick}>Scan NFC Tag</button>
+      <button id="writeButton" onClick={onWriteClick}>Write to NFC Tag</button>
+    </div>
+  );
+}
